9dfbb46852751a2f13e014a6693145e6f1083727
erdeiolver <71698306+erdeiolver@users.noreply.github.com>
2022-01-23 17:17:59 +0000

    py9.2.2

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 Írj egy programot, amely létrehoz egy 'tarolo' nevű listát, amelynek a három listaeleme egy-egy három elemű lista! Ezen beágyazott listák elemei legyenek sztring formátumban: 'O '. A program járja be a listákat, és jelenítse meg a bennük tárolt értékeket úgy, hogy azok egy 3x3-as rácsot adjanak ki. A rács képernyőn való megjelenítéséről egy eljárás gondoskodjon!
 O O O
 O O O
-O O O 
+O O O
+
+2.2 Feladat
+Egészítsd ki úgy a programot, hogy a felhasználó megadhasson egy koordinátát (a bal felső rácspont koordinátája (0;0), a jobb alsó pedig (2;2)), és ekkor a program rajzolja ki úgy a 3x3-as rácsot, hogy a megadott koordinátán 'O ' helyett, '+ ' legyen!
 '''
 
 
@@ -13,14 +16,27 @@ for i in range(3):
     tarolo.append(lista)
     for i in range(3):
         lista.append('O ')
-        
-for i in range(3):
+
+bekeres = int(input('Egyik kordináta? '))
+bekeres2 = int(input('Másik kordináta? '))
+if bekeres2 == 1:
+    bekeres += 3
+elif bekeres2 == 2:
+    bekeres += 6
+
+szamlalo2 = 0
+for i in range(3): #oszlop
     szamlalo = 0
     for i in tarolo:
-        szamlalo += 1
         for x in i:
             if szamlalo == 3:
-                print(x, end='')
+                for _ in range(3): #sor
+                    if szamlalo2 == bekeres:
+                        print('+ ', end='')
+                        szamlalo2 += 1
+                    else:
+                        print(x, end='')
+                        szamlalo2 += 1
+                    
+            szamlalo += 1
     print('')
-
-            
